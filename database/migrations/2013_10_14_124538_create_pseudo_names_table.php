54d5011bee07d2aa6bbab60443d78a8d546546ea
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-10-14 18:58:24 +0600

    chore: breeze installed with vue inertia; pseudo_names and users tables created

--- database/migrations/2013_10_14_124538_create_pseudo_names_table.php
+++ database/migrations/2013_10_14_124538_create_pseudo_names_table.php
@@ -10,11 +10,10 @@ return new class extends Migration {
    */
   public function up(): void
   {
-    Schema::create('users', function (Blueprint $table) {
+    Schema::create('pseudo_names', function (Blueprint $table) {
       $table->id();
-      $table->foreignId('pseudo_name_id')->nullable()->constrained()->nullOnDelete();
-      $table->string('password');
-      $table->rememberToken();
+      $table->string('name')->unique();
+      $table->enum('gender', ['Male', 'Female']);
       $table->timestamps();
       $table->softDeletes();
     });
@@ -25,6 +24,6 @@ return new class extends Migration {
    */
   public function down(): void
   {
-    Schema::dropIfExists('users');
+    Schema::dropIfExists('pseudo_names');
   }
 };
